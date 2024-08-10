53ec2a1970f07027efebd02d458df3d87520989a
Azazel <astrazazel@gmail.com>
2024-08-10 22:54:35 +0100

    Update config.js

--- styles/config.js
+++ styles/config.js
@@ -2,7 +2,7 @@
 /* Sheet ID
 /* Your sheet ID
 /* ------------------------------------------------------------------- */
-let sheetID = "1GwgfLizD3HQCieGia6di-TfU4E3EipT9Jb0BDZQwNak";
+let sheetID = "1WunGZrjkNILMSdnRiIwOXakFH9C4o7-GOn_YdntN7D0";
 
 
 /* ------------------------------------------------------------------- */
